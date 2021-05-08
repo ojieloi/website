fd05150c422a2493cf78e6e78cd6f6804b44043a
ojieloi <52019898+ojieloi@users.noreply.github.com>
2021-05-08 09:35:10 -0400

    Update to optimize viewing on cross platforms

--- src/components/hero.js
+++ src/components/hero.js
@@ -5,13 +5,19 @@ const Hero = () => {
     return (
         <div className='hero-container'>
             {/* Header */}
-            <h1><mark>UI/UX Designer & Front-End Developer</mark></h1>
+            <div>
+                <h1><mark>UI/UX Designer & Front-End Developer</mark></h1>
+            </div>
 
             {/* Bio */}
-            <p>Hi <span role='img' aria-label='wave'>👋</span>, I'm <span className='name'>Ike</span>. Aviation, motosport, and soccer enthusiast. Interested in all things design and development. Let’s have a conversation!</p>
+            <div>
+                <p>Hi <span role='img' aria-label='wave'>👋</span>, I'm <span className='name'>Ike</span>. Aviation, motosport, and soccer enthusiast. Interested in all things design and development. Let’s have a conversation!</p>
+            </div>
 
             {/* Resume Link */}
-            <a className='resume-link' href='https://www.dropbox.com/s/jegs86qn65z05dn/00%20-%20Resum%C3%A9.pdf?dl=0' target="_blank" rel='noreferrer'>View Resume</a>
+            <div>
+                <a className='resume-link' href='https://www.dropbox.com/s/jegs86qn65z05dn/00%20-%20Resum%C3%A9.pdf?dl=0' target="_blank" rel='noreferrer'>View Resume</a>
+            </div>
         </div>
     )
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,16 +5,18 @@ import './navbar.css'
 const Navbar = () => {
     return (
         <div className='nav'>
-            {/* Logo */}
-            <a className='logo' href='/' aria-label='Go Home'>Ik.</a>
+            <div className='nav-content'>
+                {/* Logo */}
+                <a className='logo' href='/' aria-label='Go Home'>Ik.</a>
 
-            {/* Navbar links */}
-            {/* Home, Behance, Dribbble, Resume */}
-            <div className='right-nav'>
-                <a href='https://www.instagram.com/oeki_ikeo/' target="_blank" rel='noreferrer' aria-label='Open Instagram'><SiInstagram /></a>
-                <a href='https://github.com/ojieloi' target="_blank" rel='noreferrer' aria-label='Open GitHub'><SiGithub /></a>
-                <a href='https://www.behance.net/ikeojielo' target="_blank" rel='noreferrer' aria-label='Open Behance'><SiBehance /></a>
-                <a href='https://www.linkedin.com/in/ikecojielo/' target="_blank" rel='noreferrer' aria-label='Open LinkedIn'><SiLinkedin /></a>
+                {/* Navbar links */}
+                {/* Home, Behance, Dribbble, Resume */}
+                <div className='right-nav'>
+                    <a href='https://www.instagram.com/oeki_ikeo/' target="_blank" rel='noreferrer' aria-label='Open Instagram'><SiInstagram /></a>
+                    <a href='https://github.com/ojieloi' target="_blank" rel='noreferrer' aria-label='Open GitHub'><SiGithub /></a>
+                    <a href='https://www.behance.net/ikeojielo' target="_blank" rel='noreferrer' aria-label='Open Behance'><SiBehance /></a>
+                    <a href='https://www.linkedin.com/in/ikecojielo/' target="_blank" rel='noreferrer' aria-label='Open LinkedIn'><SiLinkedin /></a>
+                </div>
             </div>
         </div>
     )
